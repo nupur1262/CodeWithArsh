b6b380c839f7edfc6625275b6f79eea022211d72
nupur1262 <64682787+nupur1262@users.noreply.github.com>
2022-01-10 21:08:54 +0530

    Create 9Is Sudoku Valid.cpp

---
+++ Amazon/9Is Sudoku Valid.cpp
@@ -0,0 +1,24 @@
+int isValid(vector<vector<int>> mat)
+    {
+        set<string>s;
+        for(int i=0;i<9;i++)
+        {
+            for(int j =0;j<9;j++)
+            {
+                if(mat[i][j]!=0)
+                {
+                int b = (i/3)*3 + (j/3);
+                string row="row" + to_string(i)+to_string(mat[i][j]);
+                string col="col" + to_string(j)+to_string(mat[i][j]);
+                string box="box" + to_string(b)+to_string(mat[i][j]);
+                if(s.find(row)!=s.end() || s.find(col)!=s.end() || s.find(box)!=s.end())
+                    return 0;
+                s.insert(row);
+                s.insert(col);
+                s.insert(box);
+                }
+            }
+        }
+        return 1;
+        // code here
+    }
